2517e699410154c69a99555c131bd8575a65d5a4
evomerse <nathandenoeux@outlook.com>
2025-10-24 19:01:19 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,19 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'public/index.html'),
-        carte: resolve(__dirname, 'public/carte.html'),
-        main: resolve(__dirname, 'public/main.html'),
-        login: resolve(__dirname, 'public/login-page.html'),
-        register: resolve(__dirname, 'public/register-page.html'),
-        alerts: resolve(__dirname, 'public/alerts-page.html'),
-        favorites: resolve(__dirname, 'public/favorites-page.html'),
-        settings: resolve(__dirname, 'public/settings-page.html')
+        index: resolve(__dirname, 'index.html'),
+        carte: resolve(__dirname, 'carte.html'),
+        main: resolve(__dirname, 'main.html'),
+        login: resolve(__dirname, 'login-page.html'),
+        register: resolve(__dirname, 'register-page.html'),
+        alerts: resolve(__dirname, 'alerts-page.html'),
+        favorites: resolve(__dirname, 'favorites-page.html'),
+        settings: resolve(__dirname, 'settings-page.html')
       }
     }
   },
   server: {
-    open: '/public/index.html',
+    open: '/index.html',
     port: 3000
   }
 });
